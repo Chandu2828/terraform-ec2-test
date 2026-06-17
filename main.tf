a9a8ba24711436845ed47abf6921a4934cd642b4
Chandu2828 <purnachand7569@gmail.com>
2026-06-17 10:37:37 +0530

    added files

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "module_test" {
     source = "../terraform-aws-instance"
     project = "roboshop"
     environment = "dev"
-    Component = "testing"
+    component = "testing"
     instance_type = "t2.micro"
     sg_ids = ["sg-0d438f72ac87797ad"]
 }
